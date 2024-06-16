f0de39b336126f3317abe8177865b5e0ca4d9bc5
Bala1884 <157810090+Bala1884@users.noreply.github.com>
2024-06-16 11:02:52 +0530

    First commit

--- server/db/connection.js
+++ server/db/connection.js
@@ -1,9 +1,11 @@
 import mongoose from "mongoose";
+import dotenv from 'dotenv';
 
+dotenv.config({path: './config.env' })
 const username=process.env.MONGODB_USERNAME;
 const password=process.env.MONGODB_PASSWORD;
 
-const conn=mongoose.connect(`mongodb+srv://Bala:2u0dAZtSUB5dUhx8@expensedetails.evbut5x.mongodb.net/?retryWrites=true&w=majority&appName=expenseDetails`)
+const conn=mongoose.connect(`mongodb+srv://${username}:${password}@expensedetails.evbut5x.mongodb.net/?retryWrites=true&w=majority&appName=expenseDetails`)
     .then(db=>{
         console.log("Database Connected");
         return db;
